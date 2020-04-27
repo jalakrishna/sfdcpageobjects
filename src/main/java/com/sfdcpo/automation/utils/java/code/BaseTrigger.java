2091068621aa3776c6549c814387b6fb1386a19d
jalakrishna <jalakrishna.alapati@gmail.com>
2020-04-27 14:37:52 -0500

    Samples_Java Learning

---
+++ src/main/java/com/sfdcpo/automation/utils/java/code/BaseTrigger.java
@@ -0,0 +1,23 @@
+package com.sfdcpo.automation.utils.java.code;
+
+public abstract class BaseTrigger extends DemoAbstractClassConstructor{
+
+	int k;
+	int l;
+	
+	public BaseTrigger(int k, int l,int i, int j){
+		super(i,j);
+		this.k = k;
+		this.l = l;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+	
+
+}
---
+++ src/main/java/com/sfdcpo/automation/utils/java/code/DemoAbstractClassConstructor.java
@@ -0,0 +1,14 @@
+package com.sfdcpo.automation.utils.java.code;
+
+public abstract class DemoAbstractClassConstructor {
+
+
+    int i;
+    int j;
+
+    public DemoAbstractClassConstructor(int i, int j){
+
+    }
+
+
+}
